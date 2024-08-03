d86a0f71573b15047c374569ad2612e59af81467
hujiacheng <hujiacheng@iipcloud.com>
2024-08-03 18:54:33 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div>Health-Guardian 后台后台</div>
+  <div>Health-Guardian 后台后台</div>
+  <div>Health-Guardian 后台后台</div>
+  <RouterView />
+</template>
