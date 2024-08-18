8a991639b3bd69102c3187ab33cfcf7007d6a33f
lin <798697193@.qq.com>
2024-08-18 21:26:20 +0800

    更改了登录页面

--- src/views/test/index.vue
+++ src/views/test/index.vue
@@ -19,10 +19,29 @@ import { onMounted } from 'vue'
 import { useRouter } from 'vue-router'
 import { getUserById } from '@/apis/user'
 const router = useRouter()
+// 张三 123456
 const handleClick = () => {
   router.push('/login')
 }
 onMounted(() => {
+  const jsonString = `{
+    "images": "string",
+    "password": "123456",
+    "permissions": -2147483648,
+    "phone": "string",
+    "status": -2147483648,
+    "username": "string",
+    "adminId": 1,
+    "adminName": "string",
+    "createBy": -2147483648,
+    "createTime": "2019-08-24T14:15:22Z",
+    "updateBy": -2147483648,
+    "updateTime": "2019-08-24T14:15:22Z"
+  }`;
+
+  const jsObject = JSON.parse(jsonString);
+  console.log(jsObject);
+
   console.log('测试页面')
   getUserById(1).then(res => {
     console.log('响应结果', res)
