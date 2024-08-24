b8e45c42206fc27ce98a6933089f2b9248aeb159
hujiacheng <hujiacheng@iipcloud.com>
2024-08-24 13:46:55 +0800

    chore: Add audio playback functionality to dashboard page

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,11 +1,18 @@
 <template>
   <div>
     dashboard
+    <el-button @click="handleClick">click</el-button>
   </div>
 </template>
 
 <script setup>
-
+import preview from '@/assets/audio/preview.mp3'
+import reminder from '@/assets/audio/reminder.mp3'
+const audio = new Audio(preview)
+const handleClick = () => {
+  console.log('click')
+  audio.play()
+}
 </script>
 
 <style lang="scss" scoped></style>
